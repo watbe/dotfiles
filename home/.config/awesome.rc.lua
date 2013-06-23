bcc332e6a42f829af1cdd4aaf34644e3d013fd1b
Wayne Tsai <wayne@tsai.co>
2013-06-23 11:05:07 +1000

    removed some unnecessary layouts

--- home/.config/awesome.rc.lua
+++ home/.config/awesome.rc.lua
@@ -100,16 +100,16 @@ altkey = "Mod1"
 -- Table of layouts to cover with awful.layout.inc, order matters.
 layouts =
 {
-    awful.layout.suit.floating,             -- 1
+--    awful.layout.suit.floating,             -- 1
     awful.layout.suit.tile,                 -- 2
     awful.layout.suit.tile.left,            -- 3
     awful.layout.suit.tile.bottom,          -- 4
-    awful.layout.suit.tile.top,             -- 5
-    awful.layout.suit.fair,                 -- 6
-    awful.layout.suit.fair.horizontal,      -- 7
-    awful.layout.suit.spiral,               -- 8
-    awful.layout.suit.spiral.dwindle,       -- 9
-    awful.layout.suit.max,                  -- 10
+--    awful.layout.suit.tile.top,             -- 5
+--    awful.layout.suit.fair,                 -- 6
+--    awful.layout.suit.fair.horizontal,      -- 7
+--    awful.layout.suit.spiral,               -- 8
+--    awful.layout.suit.spiral.dwindle,       -- 9
+--    awful.layout.suit.max,                  -- 10
     --awful.layout.suit.max.fullscreen,     -- 11
     --awful.layout.suit.magnifier           -- 12
 }
